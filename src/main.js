16a4a18d19b4d8fc4fae18f1ee8b63326019523e
Blake Curry <campbla@msn.com>
2013-06-11 19:15:22 -0400

    Added Vacuums

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 Crafty.c('PlayerCat', {
 	init: function() {
-		this.requires('Actor, Fourway, Color, 2D, Canvas, Keyboard,  Collision')
-		.fourway(3)
+		this.requires('Fourway, Color, 2D, Canvas, Keyboard,  Collision')
+		.fourway(2)
 		.color('rgb(70, 20, 70)');
 		this.bind('KeyDown', this.Shoot);
-		this.onHit('Proj', this.Spawn)
+		this.onHit('Vacuum', this.Spawn)
 	},
 	Shoot: function() {
 		if( this.isDown('SPACE')){
@@ -24,20 +24,15 @@ Crafty.c('PlayerCat', {
 
 Crafty.c('Proj', {
 	init: function() {
-		this.requires('2D, Collision, Color,  Canvas')
-		this.x=20
-		this.y=30
+		this.requires('2D, Canvas, Color, Collision')//DOM, Image,
+		//this.image('assets/img/Fish.png')
+		this.color('red')
 		this.w=10
 		this.h=10
-		this.color('red')
-		//on collide : stop existing
-		this.onHit('Collision', function() {
-			this.destroy();
-		});
 		this.bind('EnterFrame', this.Move)
 	},
 	Move: function(){
-		this.move('e', 1)
+		this.move('e', 2)
 		if (this.x<0 || this.x>800 || this.y<0 || this.y>300) {
 			this.destroy();
 		}
@@ -46,25 +41,46 @@ Crafty.c('Proj', {
 	
 });
 
-Crafty.c('Vaccum', {
+Crafty.c('Vacuum', {
 	init: function(){
-		this.requires('Collision')
+		this.requires('Color, 2D, Canvas, Collision');
+		this.w=20;
+		this.h=20;
+		this.x=850;
+		this.y=150;
+		this.color('blue');
+		this.onHit('Proj', this.Boom);
+		this.bind('EnterFrame', this.Move);
+	},
+	Move: function(){
+		this.move('w', 1)
+		if (this.x<0 || this.x>900 || this.y<0 || this.y>300) {
+			this.destroy();
+		}
+	},
+	Boom: function(hits) {
+		console.log("boom! " +hits);
+		//fireball sprite!
+		this.destroy();
+		//hits[0].destroy()
 	}
-	
 });
 
 
 Game = {
 	start: function(){
 		Crafty.init(800,300);
-		//Crafty.background('black');
-		//var space_back = Crafty.e("2D, DOM, Image").image("url('assets/img/starcat_background.png')");
 		Crafty.background("url('assets/img/starcat_background.png')"); 
 		var pc = Crafty.e('PlayerCat')
 		pc.Spawn()
-		Crafty.e('Proj')
+		pc.bind('EnterFrame', Game.spawnVac);
+	},
+	spawnVac: function(data){
+		if( data['frame'] % 100 == 0){
+			var vroom = Crafty.e('Vacuum');
+			vroom.y=Math.random()*250+25;
+		}
 	}
-
 	//logic to spawn vaccums
 
 }
