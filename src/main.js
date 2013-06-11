5d71a746b4aa51a9e1ef735c6b7ec38d1e8206a0
Alpere Fischl <aubnox@gmail.com>
2013-06-11 14:48:55 -0400

    added keyboard listener to cat

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 Crafty.c('PlayerCat', {
 	init: function() {
-		this.requires('Actor, Fourway, Color, 2D, Canvas')
+		this.requires('Actor, Fourway, Color, 2D, Canvas, Keyboard')
 		.fourway(3)
 		.color('rgb(70, 20, 70)');
+		this.bind('KeyDown', this.Shoot);
 	},
-	
+	Shoot: function() {
+		console.log("PEW PEW PEW");
+	}
 	//on collide : lots of stuff
 });
 
@@ -19,8 +22,6 @@ Crafty.c('Vaccum', {
 	//BLAKE!	 
 });
 
-Crafty.c()
-
 
 Game = {
 	start: function(){
@@ -29,8 +30,8 @@ Game = {
 		var pc = Crafty.e('PlayerCat')
 		pc.x=400
 		pc.y=300
-		pc.w=15
-		pc.h=30
+		pc.w=30
+		pc.h=20
 	}
 
 	//logic to spawn vaccums
