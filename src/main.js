31a5012ec32ef52b2438788ddb9ee9afb1ac72e4
Blake Curry <campbla@msn.com>
2013-06-11 15:31:05 -0400

    Added projectile

--- src/main.js
+++ src/main.js
@@ -1,25 +1,46 @@
 Crafty.c('PlayerCat', {
 	init: function() {
-		this.requires('Actor, Fourway, Color, 2D, Canvas, Keyboard')
+		this.requires('Actor, Fourway, Color, 2D, Canvas, Keyboard, Collision')
 		.fourway(3)
 		.color('rgb(70, 20, 70)');
 		this.bind('KeyDown', this.Shoot);
+		this.onHit('Proj', this.Spawn)
 	},
 	Shoot: function() {
 		console.log("PEW PEW PEW");
+	},
+	Spawn: function(){
+		this.x=400
+		this.y=300
+		this.w=30
+		this.h=20
 	}
 	//on collide : lots of stuff
 });
 
 Crafty.c('Proj', {
 	init: function() {
-		
-	},
-	//on collide : stop existing
+		this.requires('2D, Collision, Color, Canvas')
+		this.x=20
+		this.y=30
+		this.w=10
+		this.h=10
+		this.color('blue')
+		//on collide : stop existing
+		this.onHit('Collision', function() {
+			this.destroy();
+		});
+	}
+	
+	
+	
 });
 
 Crafty.c('Vaccum', {
-	//BLAKE!	 
+	init: function(){
+		this.requires('Collision')
+	}
+	
 });
 
 
@@ -28,10 +49,8 @@ Game = {
 		Crafty.init(800,600);
 		Crafty.background('green');
 		var pc = Crafty.e('PlayerCat')
-		pc.x=400
-		pc.y=300
-		pc.w=30
-		pc.h=20
+		pc.Spawn()
+		Crafty.e('Proj')
 	}
 
 	//logic to spawn vaccums
