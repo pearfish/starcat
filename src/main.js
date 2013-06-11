785bc13144368ba75cf3483681a62dcb7018454a
Alpere Fischl <aubnox@gmail.com>
2013-06-11 14:25:19 -0400

    added a playercat

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
+Crafty.c('PlayerCat', {
+	init: function() {
+		this.requires('Actor, Fourway, Color, 2D, Canvas')
+		.fourway(3)
+		.color('rgb(70, 20, 70)');
+	}
+});
+
 Game = {
 	start: function(){
-		Crafty.init(800,600)
-		Crafty.background('green')
+		Crafty.init(800,600);
+		Crafty.background('green');
+		var pc = Crafty.e('PlayerCat')
+		pc.x=400
+		pc.y=300
+		pc.w=10
+		pc.h=20
 	}
 }
 		
