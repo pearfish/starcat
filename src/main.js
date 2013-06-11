0991405385d7befbdc73383efe632f25643ccfab
Blake Curry <campbla@msn.com>
2013-06-11 17:54:21 -0400

    added a background

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 Crafty.c('PlayerCat', {
 	init: function() {
-		this.requires('Actor, Fourway, Color, 2D, Canvas, Keyboard, Collision')
+		this.requires('Actor, Fourway, Color, 2D, Canvas, Keyboard,  Collision')
 		.fourway(3)
 		.color('rgb(70, 20, 70)');
 		this.bind('KeyDown', this.Shoot);
@@ -8,7 +8,6 @@ Crafty.c('PlayerCat', {
 	},
 	Shoot: function() {
 		if( this.isDown('SPACE')){
-			console.log("PEW PEW PEW");
 			var p = Crafty.e('Proj');
 			p.x= this.x+30;
 			p.y = this.y;
@@ -25,24 +24,22 @@ Crafty.c('PlayerCat', {
 
 Crafty.c('Proj', {
 	init: function() {
-		this.requires('2D, Collision, Color, Canvas')
+		this.requires('2D, Collision, Color,  Canvas')
 		this.x=20
 		this.y=30
 		this.w=10
 		this.h=10
-		this.color('blue')
+		this.color('red')
 		//on collide : stop existing
 		this.onHit('Collision', function() {
 			this.destroy();
 		});
 		this.bind('EnterFrame', this.Move)
-		console.log("Ima shoot you")
 	},
 	Move: function(){
 		this.move('e', 1)
 		if (this.x<0 || this.x>800 || this.y<0 || this.y>300) {
 			this.destroy();
-			console.log("It's gone");
 		}
 	}
 	
@@ -60,7 +57,9 @@ Crafty.c('Vaccum', {
 Game = {
 	start: function(){
 		Crafty.init(800,300);
-		Crafty.background('green');
+		//Crafty.background('black');
+		//var space_back = Crafty.e("2D, DOM, Image").image("url('assets/img/starcat_background.png')");
+		Crafty.background("url('assets/img/starcat_background.png')"); 
 		var pc = Crafty.e('PlayerCat')
 		pc.Spawn()
 		Crafty.e('Proj')
